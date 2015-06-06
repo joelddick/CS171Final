424a5f7e5ba1096d329059ded0b992a0d0b4a1fc
Joel Daniel Dick <joel.d.dick@gmail.com>
2015-06-06 14:20:35 -0700

    Private Paxos

--- src/Paxos.java
+++ src/Paxos.java
@@ -1,16 +1,16 @@
 
 public class Paxos {
 
-	public int[] 	ballotNum = {0,0}; 
-	public String 	msg;
-	public int 		myVal;
-	public int		numAcks = 0;
-	public int[]	ackedAcceptBal = {0, 0}; 	// (balNum, balNumId). Store highest received ballot
-	public int		ackedAcceptVal = -1;		// and corresponding ackVal.
+	private int[] 	ballotNum = {0,0}; 
+	private String 	msg;
+	private int 		myVal;
+	private int		numAcks = 0;
+	private int[]	ackedAcceptBal = {0, 0}; 	// (balNum, balNumId). Store highest received ballot
+	private int		ackedAcceptVal = -1;		// and corresponding ackVal.
 	
-	public int[] 	acceptNum = {0,0};
-	public int 		acceptVal = -1; // Initialize to -1 because 0 is location in log
-	public int 		numAccept2s = 0;
+	private int[] 	acceptNum = {0,0};
+	private int 		acceptVal = -1; // Initialize to -1 because 0 is location in log
+	private int 		numAccept2s = 0;
 	
 	private int		siteId;
 	
@@ -22,13 +22,13 @@ public class Paxos {
 		ballotNum[1] = siteId;
 	}
 	
-	public boolean beginPhaseOne() {
+	public synchronized boolean beginPhaseOne() {
 		ballotNum[0]++;
 		sendPrepare();
 		return false;
 	}
 	
-	public boolean beginPhaseTwo() {
+	public synchronized boolean beginPhaseTwo() {
 		
 		return false;
 	}
@@ -36,21 +36,21 @@ public class Paxos {
 	/*
 	 * Leader's perspective.
 	 */
-	public void sendPrepare() {
+	public synchronized void sendPrepare() {
 		// TODO: Broadcast prepare messages to all
 	}
 	
 	/*
 	 * Cohort's perspective.
 	 */
-	public void sendAck(String ip, int port) {
+	public synchronized void sendAck(String ip, int port) {
 		// TODO: Send ack to ip:port
 	}
 	
 	/*
 	 * Leader's perspective.
 	 */
-	public void sendFirstAccept() {
+	public synchronized void sendFirstAccept() {
 		// TODO: After receiving majority acks, broadcast accept1
 		// message with ballotNum and myVal
 	}
@@ -58,7 +58,7 @@ public class Paxos {
 	/*
 	 * Everyone's perspective.
 	 */
-	public void sendSecondAccept() {
+	public synchronized void sendSecondAccept() {
 		// TODO: Broadcast accept2 with ballotNum and val to all
 	}
 	
