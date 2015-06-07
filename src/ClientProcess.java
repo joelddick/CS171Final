41b5267586ed7a124332729f5ba86503617121cd
Oliver Townsend <otownsend92@gmail.com>
2015-06-07 16:47:08 -0700

    print dat BLOGGGGG

--- src/ClientProcess.java
+++ src/ClientProcess.java
@@ -49,10 +49,10 @@ public class ClientProcess {
 			read();
 			System.out.println("Waiting for Read");
 			List<String> response = myWait();
-			System.out.println(response.get(0));
-//			for (int i = 0; i < response.size(); i++) {
-//				System.out.println(response);
-//			}
+//			System.out.println(response.get(0));
+			for (int i = 0; i < response.size(); i++) {
+				System.out.println(response);
+			}
 		}
 	}
 
