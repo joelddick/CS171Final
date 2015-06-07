c996b478236691b6dae923011038e8a28c749cd7
Oliver Townsend <otownsend92@gmail.com>
2015-06-07 13:55:50 -0700

    Reading config files

--- src/ClientProcess.java
+++ src/ClientProcess.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -8,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class ClientProcess {
 	private int leader = 0;
 	private String ipAddress = "";
@@ -16,61 +17,64 @@ public class ClientProcess {
 
 	private ServerSocket serverSocket;
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		configureGlobals();
 		ClientProcess process = new ClientProcess(args[0], Integer.valueOf(args[1]));
 	}
 
-	public ClientProcess(String ip, Integer port){
+	public ClientProcess(String ip, Integer port) {
 		ipAddress = ip;
 		this.port = port;
-		try{
-			BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
-			while (true){
+		try {
+			BufferedReader cin = new BufferedReader(new InputStreamReader(
+					System.in));
+			while (true) {
 				String input = cin.readLine();
 				processInput(input);
 			}
-		}
-		catch(IOException e){
+		} catch (IOException e) {
 			System.out.println(e.toString());
-		}	
+		}
 	}
 
-	private void processInput(String input) throws IOException{
-		if(input.length() < 4){
+	private void processInput(String input) throws IOException {
+		if (input.length() < 4) {
 			return;
 		}
 		String command = input.substring(0, 4);
-		if (command.equals("Post")){
+		if (command.equals("Post")) {
 			String message = input.substring(5);
-			if (message.length() > 140){
-				message = message.substring(0,140);
+			if (message.length() > 140) {
+				message = message.substring(0, 140);
 			}
 			post(message);
 			List<String> response = myWait();
 			System.out.println(response.get(0));
-		}
-		else if (command.equals("Read")){
+		} else if (command.equals("Read")) {
 			read();
 			List<String> response = myWait();
-			for (int i = 0; i < response.size(); i++){
+			for (int i = 0; i < response.size(); i++) {
 				System.out.println(response);
 			}
 		}
 	}
 
-	private void post(String message) throws IOException{
-		Socket socket 			= new Socket(Globals.siteIpAddresses.get(leader), Globals.sitePorts.get(leader));
-		PrintWriter socketOut 	= new PrintWriter(socket.getOutputStream(), true);
+	private void post(String message) throws IOException {
+		Socket socket = new Socket(Globals.siteIpAddresses.get(leader),
+				Globals.sitePorts.get(leader));
+		PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
 
-		socketOut.println("Post," + ipAddress + "," + port.toString() + "," + message);
+		socketOut.println("Post," + ipAddress + "," + port.toString() + ","
+				+ message);
 
 		socketOut.close();
 		socket.close();
 	}
 
-	private void read() throws IOException{
-		Socket socket 			= new Socket(Globals.siteIpAddresses.get(leader), Globals.sitePorts.get(leader));
-		PrintWriter socketOut 	= new PrintWriter(socket.getOutputStream(), true);
+	private void read() throws IOException {
+		Socket socket = new Socket(Globals.siteIpAddresses.get(leader),
+				Globals.sitePorts.get(leader));
+		PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
 
 		socketOut.println("Read," + ipAddress + "," + port.toString());
 
@@ -78,14 +82,14 @@ public class ClientProcess {
 		socket.close();
 	}
 
-	private ArrayList<String> myWait() throws IOException{
+	private ArrayList<String> myWait() throws IOException {
 		serverSocket = new ServerSocket(port);
 		Socket socket = serverSocket.accept();
 		Scanner socketIn = new Scanner(socket.getInputStream());
 
-		ArrayList <String> response = new ArrayList<String>();
+		ArrayList<String> response = new ArrayList<String>();
 
-		while (socketIn.hasNext()){
+		while (socketIn.hasNext()) {
 			response.add(socketIn.nextLine());
 		}
 
@@ -95,4 +99,37 @@ public class ClientProcess {
 
 		return response;
 	}
+
+	private static void configureGlobals() throws NumberFormatException, IOException {
+		// Read from file and get side ID
+		String path = "config.txt";
+		FileReader fr = new FileReader(path);
+		BufferedReader br = new BufferedReader(fr);
+		int myID = Integer.parseInt(br.readLine());
+		
+		String site1 = br.readLine();
+		String site2 = br.readLine();
+		String site3 = br.readLine();
+		String site4 = br.readLine();
+		String site5 = br.readLine();
+
+
+		site1 = site1.substring(0, site1.indexOf(' '));
+		site2 = site2.substring(0, site2.indexOf(' '));
+		site3 = site3.substring(0, site3.indexOf(' '));
+		site4 = site4.substring(0, site4.indexOf(' '));
+		site5 = site5.substring(0, site5.indexOf(' '));
+		
+		br.close();
+		fr.close();
+		
+		// Set globals
+		Globals.mySiteId = myID;
+		Globals.siteIpAddresses.add(site1);
+		Globals.siteIpAddresses.add(site2);
+		Globals.siteIpAddresses.add(site3);
+		Globals.siteIpAddresses.add(site4);
+		Globals.siteIpAddresses.add(site5);
+	}
+
 }
